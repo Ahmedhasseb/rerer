eefe2672e9a9424d395c23dc1339e1f05da22d9e
Ahmedhasseb <ahmedhasseb310@gmail.com>
2023-09-23 18:18:11 +0300

    add names

--- ConsoleApp1/ClassLibrary1/Class1.cs
+++ ConsoleApp1/ClassLibrary1/Class1.cs
@@ -2,6 +2,18 @@
 {
     public class Class1
     {
-        
+        public int id { get; set; }
+        public string name { get; set; }
+       public Class1(int _id,string _name)
+        {
+            id = _id;
+            name = _name;
+           
+        }
+
+        public override string ToString()
+        {
+            return $"{id} and {name}";
+        }
     }
 }
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace ConsoleApp1
+﻿using ClassLibrary1;
+namespace ConsoleApp1
 {
     class Program
     {
 public static void Main()
         {
-            Console.WriteLine( "udeyyuutr" )
+            Console.WriteLine("udeyyuutr");
+
+            Class1 csj = new Class1(0, "nMEEE");
+
+            Console.WriteLine(csj.ToString());
+          
+            
+
+
+           
         }
     }
 }
